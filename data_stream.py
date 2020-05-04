94eda396fa9d01a9d90f7af14f368b82f408b2da
Melvin Koopmans <melvin.koopmans@gmail.com>
2020-05-04 16:10:00 +0200

    read kafka topic into spark dataframe

--- data_stream.py
+++ data_stream.py
@@ -4,9 +4,21 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import pyspark.sql.functions as psf
 
-
-# TODO Create a schema for incoming resources
 schema = StructType([
+    StructField("crime_id", StringType()),
+    StructField("original_crime_type_name", StringType()),
+    StructField("report_data", TimestampType()),
+    StructField("call_date", TimestampType()),
+    StructField("offense_data", TimestampType()),
+    StructField("call_time", StringType()),
+    StructField("call_date_time", TimestampType()),
+    StructField("disposition", StringType()),
+    StructField("address", StringType()),
+    StructField("city", StringType()),
+    StructField("state", StringType()),
+    StructField("agency_id", StringType()),
+    StructField("address_type", StringType()),
+    StructField("common_location", StringType()),
 ])
 
 def run_spark_job(spark):
@@ -16,28 +28,37 @@ def run_spark_job(spark):
     # set up correct bootstrap server and port
     df = spark \
         .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("subscribe", "sf-police-department-calls-for-service") \
+        .option("startingOffsets", "earliest") \
+        .option("maxOffsetsPerTrigger", 200) \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
     # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 
-    service_table = kafka_df\
-        .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
+    service_table = kafka_df \
+        .select(psf.from_json(psf.col('value'), schema).alias("DF")) \
         .select("DF.*")
 
     # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    distinct_table = service_table.dropDuplicates(["original_crime_type_name", "disposition"])
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table.groupBy('original_crime_type_name').count()
 
     # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
     # TODO write output stream
     query = agg_df \
-
+        .writeStream \
+        .format("console") \
+        .outputMode("complete") \
+        .start()
 
     # TODO attach a ProgressReporter
     query.awaitTermination()
@@ -53,7 +74,7 @@ def run_spark_job(spark):
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
     # TODO join on disposition column
-    join_query = agg_df.
+    # join_query = agg_df.
 
 
     join_query.awaitTermination()
@@ -67,6 +88,7 @@ if __name__ == "__main__":
         .builder \
         .master("local[*]") \
         .appName("KafkaSparkStructuredStreaming") \
+        .config("spark.ui.port", 3000) \
         .getOrCreate()
 
     logger.info("Spark started")
